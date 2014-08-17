aac668c6be4296d481f54424675e1503f87c0592
Richard Fitzgerald <richard.fitzgerald36@gmail.com>
2014-08-17 18:46:24 +0100

    Added experimental binomial correction stuff, and program option to split between correctors Also renamed program as no longer computes Elo scores strictly speaking.
    --HG--
    rename : elo.cpp => rig.cpp

--- rig.cpp
+++ rig.cpp
@@ -10,9 +10,51 @@
 using namespace std;
 using elo_t = map<string,double>;// as elo_t;
 
+
+double linear_correction(double s1, double s2, int r1, int r2){
+	auto s = (double) r1/(r1+r2);
+    auto K = 16;
+    auto e = 1/(1+pow(10,(s1-s2)/400.0));
+    return K*(s-e);
+}
+
+double choose(double n, double k) {
+	double r = 1;
+	double d;
+	if (k > n) return 0;
+	for (d=1; d <= k; d++) {
+		r *= n--;
+		r /= d;
+	}
+	return r;
+}
+double binomial_pdf(int x,double p, int n){
+	return choose(n,x) * pow(p,x) * pow(1-p,n-x);
+}
+double binomial_cdf(int x,double p, int n){
+	double sum=0;
+	for(int i=0;i<x;i++){
+		sum+= binomial_pdf(i,p,n);
+
+	}
+	return sum;
+}
+
+
+double binomial_correction(double s1, double s2, int r1, int r2){
+	auto total = r1+r2;
+    auto p = 1/(1+pow(10,(s1-s2)/400.0));
+    auto K = 32;
+
+    double cor;
+	cor=binomial_cdf(r1,p,total)+binomial_pdf(r1,p,total)/2-0.5;
+    return K*cor;
+}
+
 void update_elo(elo_t &elo_map,
-                const string& k1, const string& k2,
-                double s, double rounds=1){
+                const std::string& k1, const std::string& k2,
+                int s, int r,
+                double (*corrector)(double,double,int,int)){
     if(!elo_map.count(k1))
         elo_map[k1]=1400.0;
 
@@ -21,18 +63,16 @@ void update_elo(elo_t &elo_map,
 
     auto s1 = elo_map[k1];
     auto s2 = elo_map[k2];
-    s=s/rounds;
-    auto K = 16;
-    auto e = 1/(1+pow(10,(s1-s2)/400.0));
 
-    elo_map[k1] = s1 + K*(s-e);
-    elo_map[k2] = s2 + K*(e-s);
+    elo_map[k1] += corrector(s1,s2,s,r-s);
+    elo_map[k2] += corrector(s2,s1,r-s,s);
 }
 
 struct opt_t{
 	istream* in;
 	ostream* out;
-	opt_t():in(&cin),out(&cout){};
+	double (*cor)(double,double,int,int);
+	opt_t():in(&cin),out(&cout),cor(&linear_correction){};
 	~opt_t(){
 		if(in!=&cin){
 			delete in;
@@ -71,6 +111,12 @@ void read_options(opt_t& opt,int iargs,const char ** arg){
 				exit(1);
 			}
 		}
+		else if(strcmp("-b",arg[i])==0){
+			opt.cor=&binomial_correction;
+		}
+		else if(strcmp("-l",arg[i])==0){
+			opt.cor=&linear_correction;
+		}
 		else{
 			cerr<<"Unrecognised option '"<<arg[i]<<"'"<<endl;
 			exit(1);
@@ -92,15 +138,21 @@ int main(int iargs, const char ** args) {
 		smatch matches;
 
 		if(regex_match(line, matches, names_format)){
-            update_elo(elo_map,matches[1].str(),matches[2].str(),1);
+            update_elo(elo_map,
+                       matches[1].str(),matches[2].str(),
+                       1,1,
+                       opt.cor);
 		}
         else if(regex_match(line,matches, names_and_score_format)){
 	        auto score = stoi(matches[2].str());
 	        auto rounds= stoi(matches[3].str()) + score;
-	        update_elo(elo_map,matches[1].str(),matches[4].str(),score,rounds);
+	        update_elo(elo_map,
+	                   matches[1].str(),matches[4].str(),
+	                   score,rounds,
+	                   opt.cor);
         }
         else{
-			cerr<< "Datafile in incorrect format."<<endl<<line;
+			cerr<< "Datafile in incorrect format."<<endl;
 			exit(1);
 		}
 
