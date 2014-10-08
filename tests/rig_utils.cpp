dbcb7a5cd1fc681c2d52336fc611b1323a42d262
Richard Fitzgerald <richard.fitzgerald36@gmail.com>
2014-10-08 15:36:34 +0100

    Increased test coverage for expected_outcome.

--- tests/rig_utils.cpp
+++ tests/rig_utils.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+bool approx_equal(double x,double y){
+	double delta = 1e-10;
+	return x < y + delta && y < x + delta;
+}
+
 void cor_tests(double (*cor)(double, double, int,int)){
 
 	random_device rd;
@@ -11,14 +16,12 @@ void cor_tests(double (*cor)(double, double, int,int)){
 	normal_distribution<double> elo_dist(1400,2000/7.0);
 	uniform_int_distribution<int> result_dist(0,5);
 
-	double delta = 1e-10;
 
 	SECTION("Corrections must be symmetric."){
 		for(int i=0;i<100;i++){
 			double elo1=elo_dist(gen), elo2=elo_dist(gen);
 			int r1=result_dist(gen), r2=5-r1;
-			REQUIRE(cor(elo1,elo2,r1,r2) < (-cor(elo2,elo1,r2,r1) + delta));
-			REQUIRE((cor(elo1,elo2,r1,r2) + delta) > -cor(elo2,elo1,r2,r1));
+			REQUIRE(approx_equal(cor(elo1,elo2,r1,r2),-cor(elo2,elo1,r2,r1)));
 		}
 	}
 
@@ -37,7 +40,11 @@ TEST_CASE("Expected outcome behaves sensibly","[expected_outcome]"){
 		REQUIRE(expected_outcome(1024,1000) > 0.5);
 		REQUIRE(expected_outcome(987,1987) < 0.5);
 	}
+	SECTION("Equal Elo scores results in draw"){
+		REQUIRE(approx_equal(expected_outcome(1321,1321),0.5));
+	}
 }
+
 TEST_CASE("Matching names works as a equality operator","[match_name]"){
 	SECTION("Identical names return true"){
 		REQUIRE(match_name("Player one","Player one")==true);
