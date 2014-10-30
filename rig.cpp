22d6fd17fa2e7c273ef46292a025f5395fc87a9a
Richard Fitzgerald <richard.fitzgerald36@gmail.com>
2014-10-30 12:12:06 +0000

    Neaten up main().

--- rig.cpp
+++ rig.cpp
@@ -8,58 +8,23 @@
 #include "prediction_methods.h"
 
 using namespace std;
-int main(int iargs, const char ** args) {
-	// Get options from command line
-	opt_t opt = read_options(iargs,args);
 
-	// Generate a list of match outcomes from input
+results_t get_results(const opt_t& opt){
 	results_t results;
-	try{
-		switch(opt.in_fmt){
-		case opt_t::IN_TREE:
-			results=parse_tree(opt.in);
-			break;
-		case opt_t::IN_LIST:
-			results=parse_list(opt.in);
-			break;
-		default:
-			throw runtime_error("Unimplemented Input Format.");
-		}
-	}
-	catch (std::exception& e){
-		cout<< "Parsing error! Error was:" << endl
-		    << e.what() << endl;
-		return 1;
-	}
-
-	prediction_method* predictor = nullptr;
-	if(opt.out_fmt != opt_t::OUT_LIST){
-		switch(opt.method){
-			case opt_t::ELO_METHOD:
-				predictor = new elo(opt.cor);
-		}
-	}
-
-	// Calculate Elo Ratings
-	score_t scores;
-	if(predictor != nullptr){
-		scores = (*predictor)(results);
-	}
-
-	// Output in relevant format
-	switch(opt.out_fmt){
-	case opt_t::OUT_ELO:
-		for(auto k:scores){
-			*(opt.out)<<k.second<<"\t"<<k.first<<std::endl;
-		}
+	switch(opt.in_fmt){
+	case opt_t::IN_TREE:
+		results=parse_tree(opt.in);
 		break;
-	case opt_t::OUT_LIST:
-		for(auto& k:results){
-			*(opt.out)<< k.name1 << " " << k.score1 <<" - "
-			          << k.score2<< " " << k.name2 << std::endl;
-		}
+	case opt_t::IN_LIST:
+		results=parse_list(opt.in);
 		break;
-	case opt_t::OUT_COMP:{
+	default:
+		throw runtime_error("Unimplemented Input Format.");
+	}
+	return results;
+}
+
+void do_comparison(opt_t& opt,score_t scores){
 		// If comparing two players
 		// Get relevant players
 		auto player1 = find_player(scores,opt.player1);
@@ -67,12 +32,11 @@ int main(int iargs, const char ** args) {
 
 		// Check players exist
 		if(player1 == scores.end()){
-			std::cerr << "Could not find player '" <<opt.player1<<"'"<<std::endl;
-			return 1;
+			throw runtime_error("Could not find player '" + opt.player1 + "'");
 		}
+
 		if(player2 == scores.end()){
-			std::cerr << "Could not find player '" <<opt.player2<<"'"<<std::endl;
-			return 1;
+			throw runtime_error("Could not find player '" + opt.player2 + "'");
 		}
 
 
@@ -90,8 +54,68 @@ int main(int iargs, const char ** args) {
 			e = 1-e;
 		}
 
-		// Output result
-		*(opt.out)<< player1->first << " to beat " << player2->first << " (" << (e*100) << "%)"<<std::endl;
+		*(opt.out)<< player1->first << " to beat " << player2->first
+		          << " (" << (e*100) << "%)" << endl;
+}
+
+int main(int iargs, const char ** args) {
+	// Get options from command line
+	opt_t opt = read_options(iargs,args);
+
+	bool have_results = opt.in_fmt != opt_t::IN_ELO;
+	bool need_elo = opt.out_fmt != opt_t::OUT_LIST;
+
+	results_t results;
+	score_t scores;
+
+	if(have_results){
+		// Generate a list of match outcomes from input
+		try{
+			results = get_results(opt);
+		}
+		catch (std::exception& e){
+			cerr << "Parsing error! Error was:" << endl
+				 << e.what() << endl;
+			return 1;
+		}
+
+		//Use correct prediction method
+		prediction_method* predictor = nullptr;
+		if(opt.out_fmt != opt_t::OUT_LIST){
+			switch(opt.method){
+				case opt_t::ELO_METHOD:
+					predictor = new elo(opt.cor);
+			}
+		}
+
+		// Calculate Elo Ratings
+		if(need_elo && predictor != nullptr){
+			scores = (*predictor)(results);
+		}
+	}
+
+
+	// Output in relevant format
+	switch(opt.out_fmt){
+	case opt_t::OUT_ELO:
+		for(auto k:scores){
+			*(opt.out) << k.second << "\t" << k.first << endl;
+		}
+		break;
+	case opt_t::OUT_LIST:
+		for(auto& k:results){
+			*(opt.out) << k.name1  << " " << k.score1 <<" - "
+			           << k.score2 << " " << k.name2  << endl;
+		}
+		break;
+	case opt_t::OUT_COMP:
+		try{
+			do_comparison(opt,scores);
+		}
+		catch (std::exception& e){
+			cerr << "Could not perform player comparison. Error was: "<< endl
+			     << e.what() << endl;
+			return 1;
 		}
 		break;
 	default:
